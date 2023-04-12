cd9ebb7e3da87fc2f7ce9606e8bd6cf7fee167ea
infected_ <zzitoz@gmail.com>
2023-04-12 14:46:20 +0100

    update v08

--- fast-oss.sh
+++ fast-oss.sh
@@ -6,20 +6,20 @@ export ZIP_DIR="/mnt/Android/anykernel"
 export ZIP_OUT_DIR="/mnt/Android/Out_Zips"
 rm -rfv /mnt/Android/anykernel/Image.gz
 rm -rfv /mnt/Android/anykernel/dtbo.img
-#make O=out clean
-#make O=out mrproper
-#rm -rfv out
+make O=out clean
+make O=out mrproper
+rm -rfv out
 export PATH="/mnt/Bathtube/toolchains/neutron-clang/bin:$PATH"
 export USE_CCACHE=1
 export CLANG_PATH="/mnt/Bathtube/toolchains/neutron-clang/bin/clang"
 export ARCH=arm64
-export VARIANT="aurora-hell-r05"
+export VARIANT="aurora-hell-r07"
 export HASH=`git rev-parse --short=6 HEAD`
 export KERNEL_ZIP="$VARIANT-$HASH"
 export LOCALVERSION="~'$VARIANT-$HASH'"
 export KBUILD_COMPILER_STRING=$($CLANG_PATH --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-make $KBUILD_OUTPUT CC=clang LLVM=1 LLVM_IAS=1 vendor/kona-perf_defconfig
-make $KBUILD_OUTPUT CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 -j32 CROSS_COMPILE=/mnt/Bathtube/toolchains/gcc-linaro-12.2.1-2023.03-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=/mnt/Bathtube/toolchains/gcc-linaro-12.2.1-2023.03-aarch64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+make O=out CC=clang LLVM=1 LLVM_IAS=1 vendor/kona-perf_defconfig
+make O=out CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 -j32 CROSS_COMPILE=/mnt/Bathtube/toolchains/gcc-linaro-12.2.1-2023.03-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=/mnt/Bathtube/toolchains/gcc-linaro-12.2.1-2023.03-aarch64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 cp -v $KBUILD_OUTPUT/arch/arm64/boot/Image.gz $ZIP_DIR/Image.gz
 cp -v $KBUILD_OUTPUT/arch/arm64/boot/dtbo.img $ZIP_DIR/dtbo.img
 cd $ZIP_DIR
