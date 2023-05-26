99e277e524b474bfea8d94c9025e91fb451ed033
infected_ <zzitoz@gmail.com>
2023-05-26 22:01:09 +0100

    adapt deepongi

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= Meteoric kernel by HELLBOY017
+kernel.string= Aurora hell r10 kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
